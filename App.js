6fe02466b9e051df19b9986f9a8cde9657ff41cb
haruto.watanabe <w.haru10n@gmail.com>
2019-01-20 21:27:51 +0900

    todo追加機能の実装

--- App.js
+++ App.js
@@ -6,45 +6,80 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+} from 'react-native';
+import TodoInput from './src/component/TodoInput';
+import TodoItem from './src/component/TodoItem';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component<{}> {
+  constructor(props) {
+    super(props);
 
-type Props = {};
-export default class App extends Component<Props> {
+    this.state = {
+      list: [],
+    };
+  }
+  
+  onPress = (text) => {
+    const list = [].concat(this.state.list);
+
+    list.push({
+      key: Date.now(),
+      text: text,
+      done: false,
+    });
+
+    this.setState({
+      list,
+    });
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>React Nativeへようこそ!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+      const {
+        list,
+      } = this.state;
+
+      return (
+        <View style={StyleSheet.container}>
+          <View style={StyleSheet.main}>
+            <TodoInput onPress={this.onPress}/>
+            <View style={StyleSheet.todoListContainer}>
+              <FlatList
+                style={styles.todoList}
+                data={list}
+                renderItem={({ item }) => <TodoItem {...item} />}
+              />
+            </View>
+          </View>
+        </View>
+      );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    paddingTop: 40,
     alignItems: 'center',
-    backgroundColor: '#333',
+    backgroundColor: '#ecf0f1',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    color: '#FFF',
-    margin: 10,
+  main: {
+    flex: 1,
+    maxWidth: 400,
+    alignItems: 'center',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#FFF',
-    marginBottom: 5,
+  todoListContainer: {
+    flexDirection: 'row',
+    flex: 1,
   },
+  todoList: {
+    paddingLeft: 10,
+    paddingRight: 10,
+  }
 });
